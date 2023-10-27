d2c4e64fdfeb0f530c1208e289f7ad98fca3c3b6
BEMG-DEVELOP <78712696+BEMG-DEVELOP@users.noreply.github.com>
2023-10-27 19:54:09 +0000

    Uso de las funciones con argumentos.
    Se modifico el código a modo que se usen argumentos y al momento de llamar a las funciones se especifiquen los valores.

--- index.php
+++ index.php
@@ -1,31 +1,23 @@
 <?php
-function suma(){
-  $valor1 = 8;
-  $valor2 = 10;
+function suma($valor1,$valor2){
   $resultado = $valor1 + $valor2;
  print_r("La suma es:".$resultado."<br>") ;
 }
-function resta(){
-  $valor1 = 10;
-  $valor2 = 5;
+function resta($valor1,$valor2){
   $resultado = $valor1 - $valor2;
   print_r("La resta es:".$resultado."<br>");
 }
-function multiplicacion(){
-  $valor1 = 3;
-  $valor2 = 10;
+function multiplicacion($valor1,$valor2){
   $resultado = $valor1 * $valor2;
   print_r("La multiplicación es:".$resultado."<br>");
 }
-function division(){
-  $valor1 = 10;
-  $valor2 = 2;
+function division($valor1,$valor2){
   $resultado =  $valor1 / $valor2;
   print_r("La división es:".$resultado."<br>");
 }
 
-suma();
-resta();
-multiplicacion();
-division();
+suma(10,2);
+resta(10,2);
+multiplicacion(10,2);
+division(10,2);
 ?>
