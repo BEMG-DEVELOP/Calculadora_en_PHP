4e175a7d671924000a9a4624166462a4a51d6e67
BEMG-DEVELOP <78712696+BEMG-DEVELOP@users.noreply.github.com>
2023-10-27 20:41:06 +0000

    Uso de switch anidados
    Este fue el resultado del desarrollo al algoritmo de recomendación de ciudad, según su clima, ubicación o atracción turistica.

--- index.php
+++ index.php
@@ -20,17 +20,56 @@ function busqueda($interes,$caracteristica){
   
   switch($interes){
     case"clima":
-      print_r("Estas en el caso 1 ".$caracteristica);
+      switch($caracteristica){
+        case"frío":
+          $busqueda = array_search("frío",$clima);
+        break;
+        case"calido":
+          $busqueda = array_search("calido",$clima);
+        break;
+        case "templado":
+          $busqueda = array_search("templado",$clima);
+        break;
+      }
+        print_r("La ciudad que te recomendamos es: ".$busqueda);
     break;
     case "ubicacion":
-     print_r("Estas en el caso 2 ".$caracteristica);
+    switch($caracteristica){
+        case"norte":
+          $busqueda = array_search("norte",$ubicacion);
+        break;
+        case"sur":
+          $busqueda = array_search("sur",$ubicacion);
+        break;
+        case "este":
+          $busqueda = array_search("este",$ubicacion);
+        break;
+        case "oeste":
+          $busqueda = array_search("oeste",$ubicacion);
+        break;
+    }
+      print_r("La ciudad que te recomendamos es: ".$busqueda);
     break;
     case "turismo":
-     print_r("Estas en el caso 3 ".$caracteristica);
+    switch($caracteristica){
+        case"mar":
+          $busqueda = array_search("mar",$turismo);
+        break;
+        case"llano":
+          $busqueda = array_search("llano",$turismo);
+        break;
+        case "desierto":
+          $busqueda = array_search("desierto",$turismo);
+        break;
+        case "valle":
+          $busqueda = array_search("valle",$turismo);
+        break;
+    }
+      print_r("La ciudad que te recomendamos es: ".$busqueda);
     break;
   }
 }
-#busqueda("clima","templado");
-#busqueda("ubicacion","norte");
+busqueda("clima","templado");
+busqueda("ubicacion","norte");
 busqueda("turismo","mar");
 ?>
