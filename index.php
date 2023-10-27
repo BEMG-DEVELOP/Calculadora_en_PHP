3a52233b953a035f7718e869a41703da2936b8d4
BEMG-DEVELOP <78712696+BEMG-DEVELOP@users.noreply.github.com>
2023-10-27 19:49:31 +0000

    Funciones básicas
    Se crearon las funciones básicas para los distintos procesos de operaciones aritmeticas.

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
 <?php
+function suma(){
+  $valor1 = 8;
+  $valor2 = 10;
+  $resultado = $valor1 + $valor2;
+ print_r("La suma es:".$resultado."<br>") ;
+}
+function resta(){
+  $valor1 = 10;
+  $valor2 = 5;
+  $resultado = $valor1 - $valor2;
+  print_r("La resta es:".$resultado."<br>");
+}
+function multiplicacion(){
+  $valor1 = 3;
+  $valor2 = 10;
+  $resultado = $valor1 * $valor2;
+  print_r("La multiplicación es:".$resultado."<br>");
+}
+function division(){
+  $valor1 = 10;
+  $valor2 = 2;
+  $resultado =  $valor1 / $valor2;
+  print_r("La división es:".$resultado."<br>");
+}
 
+suma();
+resta();
+multiplicacion();
+division();
 ?>
