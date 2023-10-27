17088df21b87b203e04ad9c3fb6acd68075d8c18
BEMG-DEVELOP <78712696+BEMG-DEVELOP@users.noreply.github.com>
2023-10-27 20:49:28 +0000

    Simplificación del mensaje de respuesta.
    Se simplifico las líneas de código de las respuestas al finalizar una operación aritmetica

--- calculadora.php
+++ calculadora.php
@@ -3,27 +3,24 @@ function calculadora($valor1,$operador,$valor2){
   switch($operador){
     case "+":
       $resultado = $valor1 + $valor2;
-      print_r("El resultado de la suma es: ".$resultado."<br>");
     break;
     case "-":
       $resultado = $valor1 - $valor2;
-      print_r("El resultado de la resta es: ".$resultado."<br>");
     break;
     case "*":
       $resultado = $valor1 * $valor2;
-    print_r("El resultado de la multiplicacion es: ".$resultado."<br>");
     break;
     case "/":
       $resultado = $valor1 / $valor2;
-    print_r("El resultado de la división es: ".$resultado."<br>");
     break;
     default:
     print_r("La operación no es valida");
     break;
   }
+  print_r("El resultado de la operación es: ".$resultado."<br>");
 }
 
-calculadora(10,"+",2);
+calculadora(10,"%",2);
 calculadora(10,"-",2);
 calculadora(10,"*",2);
 calculadora(10,"/",2);
